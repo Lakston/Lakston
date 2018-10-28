389ceacc301b6566e666a33883985d36c14bdf58
Fabrice_Pomata <fab.pomata@gmail.com>
2018-10-28 19:54:33 +0100

    refacto

--- src/components/gallery/GridItem.jsx
+++ src/components/gallery/GridItem.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 
 const GridItem = ({ gallery, index, columns }) => {
   const url = `${process.env.PUBLIC_URL}/images/${gallery}/thumbnail-${index}.jpg`
-  const style = {
-    cursor: 'pointer',
-    objectFit: 'cover'
+
+  const styles = {
+    container: {
+      width: `calc(${100 / columns}% - 1rem)`,
+      margin: '0 0.5rem 0.25rem 0.5rem'
+    },
+    gridItem: {
+      width: '100%',
+      paddingBottom: '100%',
+      background: `url(${url}) center center / cover no-repeat`,
+      margin: '.5rem'
+    }
   }
+
   return (
-    <div
-      style={{
-        width: `calc(${100 / columns}% - 1rem)`,
-        height: '200px',
-        background: `url(${url}) center center / cover no-repeat`,
-        margin: '.5rem',
-        paddingBottom: '50%'
-      }}
-    >
-      {/* <img src={url} alt="" style={style} /> */}
+    <div style={styles.container}>
+      <div style={styles.gridItem} />
     </div>
   )
 }
